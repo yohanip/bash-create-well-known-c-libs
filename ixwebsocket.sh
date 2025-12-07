c73ce5ff380960d318825f55065fe436583ea386
laptop <laptop@laptop.com>
2025-12-07 23:54:33 +0700

    fix ixwebsocket for android

--- ixwebsocket.sh
+++ ixwebsocket.sh
@@ -6,16 +6,8 @@ COMMON_FLAGS=(
     '-DUSE_TLS=ON'
     '-DUSE_MBED_TLS=ON'
 )
-MBED_TLS_DEFS=(
-    '-DMBEDTLS_LIBRARY=mbedtls'
-    '-DMBEDX509_LIBRARY=mbedx509'
-    '-DMBEDCRYPTO_LIBRARY=mbedcrypto'
-    '-DMBEDTLS_VERSION_GREATER_THAN_3=ON'
-)
 
-# we are not using tls for debug build
-./lib.sh "../$THE_FOLDER" -DBUILD_SHARED_LIBS=ON "${COMMON_FLAGS[@]}"
-./lib.sh "../$THE_FOLDER" --release "${COMMON_FLAGS[@]}"
-# we dont build for emscripten, will use emscripten websocket library
-./lib.sh "../$THE_FOLDER" --release --android "${COMMON_FLAGS[@]}" "${MBED_TLS_DEFS[@]}" -DMBEDTLS_INCLUDE_DIRS=install/android/Release/arm64-v8a/include
-./lib.sh "../$THE_FOLDER" --release --android arm "${COMMON_FLAGS[@]}" "${MBED_TLS_DEFS[@]}" -DMBEDTLS_INCLUDE_DIRS=install/android/Release/armeabi-v7a/include
+./lib.sh "../$THE_FOLDER" -DBUILD_SHARED_LIBS=ON "${COMMON_FLAGS[@]}" -DCMAKE_PREFIX_PATH=/home/yohanip/Documents/c++/well-known/cmaker/install/linux/Release/lib
+./lib.sh "../$THE_FOLDER" --release "${COMMON_FLAGS[@]}" -DCMAKE_PREFIX_PATH=/home/yohanip/Documents/c++/well-known/cmaker/install/linux/Release/lib
+./lib.sh "../$THE_FOLDER" --release --android "${COMMON_FLAGS[@]}" -DCMAKE_FIND_ROOT_PATH=/home/yohanip/Documents/c++/well-known/cmaker/install/android/Release/arm64-v8a
+./lib.sh "../$THE_FOLDER" --release --android arm "${COMMON_FLAGS[@]}" -DCMAKE_FIND_ROOT_PATH=/home/yohanip/Documents/c++/well-known/cmaker/install/android/Release/armeabi-v7a
