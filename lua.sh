27f0fa8432a526ebe95924f1c80456de89f6132b
laptop <laptop@laptop.com>
2025-12-30 23:50:00 +0700

    built lua for desktop, mobile, and emscripten

--- lua.sh
+++ lua.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 ./lib.sh the_lua -DBUILD_SHARED_LIBS=ON 
-# ./lib.sh the_lua --release 
-# ./lib.sh the_lua --release --ems
-# ./lib.sh the_lua --release --android
-# ./lib.sh the_lua --release --android arm
+./lib.sh the_lua --release 
+./lib.sh the_lua --release --ems
+./lib.sh the_lua --release --android
+./lib.sh the_lua --release --android arm
