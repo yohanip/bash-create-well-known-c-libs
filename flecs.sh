b77d4e2bae6f60582c79ce0449876f5663314d7c
laptop <laptop@laptop.com>
2025-12-30 23:42:04 +0700

    adding lua and angelscript

--- flecs.sh
+++ flecs.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 ./lib.sh ../flecs -DFLECS_STATIC=OFF -DFLECS_SHARED=ON
 ./lib.sh ../flecs -DFLECS_STATIC=ON -DFLECS_SHARED=OFF --release
+# ./lib.sh ../flecs -DFLECS_STATIC=ON -DFLECS_SHARED=OFF --release --ems -DCMAKE_C_FLAGS='-DFLECS_CUSTOM_BUILD -DFLECS_CPP -DFLECS_SYSTEM -DFLECS_LOG -DFLECS_PIPELINE -DFLECS_META'
 ./lib.sh ../flecs -DFLECS_STATIC=ON -DFLECS_SHARED=OFF --release --ems
 ./lib.sh ../flecs -DFLECS_STATIC=ON -DFLECS_SHARED=OFF --release --android
 ./lib.sh ../flecs -DFLECS_STATIC=ON -DFLECS_SHARED=OFF --release --android arm
