38de2eb3347b25c8785e30b6d12e8b9da2111bb4
yohanip <yohanipbdg@gmail.com>
2026-01-01 18:21:06 +0700

    built lua for windows

---
+++ README.md
@@ -0,0 +1,3 @@
+# for lua
+- download the lua source
+- mklink \D the_lua\source _path_to_the_lua_sources_
--- lua.sh
+++ lua.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 set -euo pipefail
+unix_based=false
+case "$(uname)" in
+  Linux*)   unix_based=true ;;
+  Darwin*)  unix_based=true ;;
+  MINGW*|MSYS*|CYGWIN*) unix_based=false ;;
+  *)        echo "Unknown OS" ;;
+esac
 
-./lib.sh the_lua -DBUILD_SHARED_LIBS=ON 
+if [[ "$unix_based" == true ]]; then
+    ./lib.sh the_lua -DBUILD_SHARED_LIBS=ON 
+else
+    ./lib.sh the_lua
+fi
 ./lib.sh the_lua --release 
 ./lib.sh the_lua --release --ems
 ./lib.sh the_lua --release --android
